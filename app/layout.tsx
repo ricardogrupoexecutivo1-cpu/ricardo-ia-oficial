6f1a129987553f5e06eaee60636eb41d81c81303
Ricardo Leonardo <ricardogrupoexecutivo1@gmail.com>
2026-03-09 13:44:44 -0300

    add google search console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,14 +15,23 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+
+        <meta name="google-site-verification" content="0uT9i8gslJnvaJp_r8qNAqzIgMXR748bwAqv8v6lFag" />
+
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#111111" />
+
         <link rel="apple-touch-icon" href="/aurora-icon-192.png" />
+
+        <title>Aurora IA</title>
+
       </head>
+
       <body>
         {children}
         <Analytics />
       </body>
+
     </html>
   )
 }
