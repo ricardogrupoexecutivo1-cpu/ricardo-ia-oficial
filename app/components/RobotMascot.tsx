e406450a5502c92169022aec529f7ef06895d3c4
Ricardo Leonardo <ricardogrupoexecutivo1@gmail.com>
2026-03-13 10:05:47 -0300

    add aurora mascot image

---
+++ app/components/RobotMascot.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function RobotMascot(){
+
+return(
+
+<div
+style={{
+position:"fixed",
+bottom:"40px",
+right:"40px",
+fontSize:"60px",
+animation:"float 4s ease-in-out infinite"
+}}
+>
+🤖
+</div>
+
+)
+
+}
